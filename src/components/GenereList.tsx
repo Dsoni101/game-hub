afcc399bfd4782fa30aaf956b1f593fcb34e3ebf
Divyesh Soni <divyeshsoni.101@gmail.com>
2025-09-24 11:07:09 +0200

    Fetching and caching genres

--- src/components/GenereList.tsx
+++ src/components/GenereList.tsx
@@ -28,7 +28,7 @@ const GenereList = ({ selectedGenre, onSelectGenere }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
